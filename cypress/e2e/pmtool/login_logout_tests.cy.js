3ac4a8343f69d0517239d6e69f1cdf8db2a5cfaa
Sabina Paříková <hamanovas@gmail.com>
2023-10-17 20:21:49 +0200

    testy page objects model and faker

---
+++ cypress/e2e/pmtool/login_logout_tests.cy.js
@@ -0,0 +1,24 @@
+import { HomePage } from "../../page-objects/pmtool/home_page";
+import { LoginPage } from "../../page-objects/pmtool/login_page";
+
+describe("Login and Logout Tests", () => {
+  it("Login to pmtool", () => {
+    const loginPage = new LoginPage();
+    const homePage = new HomePage();
+    loginPage.openPmtool();
+    loginPage.typeUsername("cy_podzim_2023");
+    loginPage.typePassword("CypressPodzim");
+    loginPage.clickLogin();
+  });
+
+  it("Login and logout pmtool", () => {
+    const loginPage = new LoginPage();
+    const homePage = new HomePage();
+    loginPage.openPmtool();
+    loginPage.typeUsername("cy_podzim_2023");
+    loginPage.typePassword("CypressPodzim");
+    loginPage.clickLogin();
+    homePage.clickProfileSec();
+    homePage.clickLogout();
+  });
+});
