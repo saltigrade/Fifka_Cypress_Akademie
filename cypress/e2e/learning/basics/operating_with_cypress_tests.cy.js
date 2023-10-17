e2c03952855206b6eb3c9dc65a2955af18f547c7
Sabina Paříková <hamanovas@gmail.com>
2023-10-17 18:06:56 +0200

    prvni testy cypress

---
+++ cypress/e2e/learning/basics/operating_with_cypress_tests.cy.js
@@ -0,0 +1,14 @@
+describe("Operating with Cypress Test", () => {
+  it("Login test", () => {
+    cy.visit("http://tredgate.com/pmtool/");
+    cy.get("#username").type("cy_podzim_2023");
+    cy.get("#password").type("CypressPodzim");
+    cy.get(".btn").click();
+  });
+
+  // ! BUG id: teg001
+  it.skip("Test Failure - element not present", () => {
+    cy.visit("http://tredgate.com/pmtool/");
+    cy.get("#username2");
+  });
+});
