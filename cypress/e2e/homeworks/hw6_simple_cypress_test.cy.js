a5f76c5cec7f37ab582c1d07c8b1399b78a55b8f
Sabina Paříková <hamanovas@gmail.com>
2023-10-17 22:42:56 +0200

    hw6_simple_cypress_test_using_variables_trying_faker

---
+++ cypress/e2e/homeworks/hw6_simple_cypress_test.cy.js
@@ -0,0 +1,30 @@
+import { faker } from "@faker-js/faker";
+
+describe("Simple cypress test with faker", () => {
+  it("Fill registration with faker", () => {
+    const firstName = faker.person.firstName();
+    const lastName = faker.person.lastName();
+    const email = faker.internet.exampleEmail();
+    const telephone = faker.phone.number();
+    const password = "NeumimPouzitFaker";
+    const confirm = "NeumimPouzitFaker";
+    cy.log("FirstName: " + firstName);
+    cy.log("LastName: " + lastName);
+    cy.log("Email: " + email);
+    cy.log("Telephone: " + telephone);
+    cy.log("Password: " + password);
+    cy.log("Confirm password: " + confirm);
+    cy.visit("http://tredgate.com/eshop/index.php?route=account/register");
+    cy.get("#input-firstname").type(firstName);
+    cy.get("#input-lastname").type(lastName);
+    cy.get("#input-email").type(email);
+    cy.get("#input-telephone").type(telephone);
+    cy.get("#input-password").type(password);
+    cy.get("#input-confirm").type(confirm);
+    cy.get(".pull-right > .btn").click();
+  });
+});
+
+/* Dotaz na password a confirm password pomocí Faker. Generuje se pokaždé nové heslo. 
+Dá se udělat, aby se zkopírovalo náhodně vygenerované heslo, abych mohla použít faker?
+ */
