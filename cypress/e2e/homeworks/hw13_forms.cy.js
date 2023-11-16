11c2452e9f83748634036ed5d4e68012c4ff9ab7
Sabina Paříková <hamanovas@gmail.com>
2023-11-16 15:25:39 +0100

    testing until zipcode ready, without shoud, not finished

--- cypress/e2e/homeworks/hw13_forms.cy.js
+++ cypress/e2e/homeworks/hw13_forms.cy.js
@@ -6,9 +6,23 @@ describe("Automation test store - registration", () => {
     new HomePage().openStore();
   });
   it("Login or register and create account", () => {
-    // const email = faker.internet.exampleEmail();
+    const email = faker.internet.exampleEmail();
     // let username = Cypress.env("automationteststore_username");
     // let password = Cypress.env("automationteststore_password");
-    new HomePage().clickLoginRegisterButton().clickContinueButton();
+    new HomePage()
+      .clickLoginRegisterButton()
+      .clickContinueButton()
+      .typeFirstName("Sabina")
+      .typeLastName("Parik")
+      .typeEmail(email)
+      .typeTelephone("+420728759255")
+      .typeFax("123456")
+      .typeCompany("Microsoft")
+      .typeAddress1("Pražská 1")
+      .typeAddress2("Pražská 2")
+      .typeCity("Praha")
+      .selectCountry("Czech Republic")
+      .selectRegionState("Vysocina")
+      .typeZipCode("18100");
   });
 });
