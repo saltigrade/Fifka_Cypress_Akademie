e848f03dc9e76177015c194ff1dba8c5db6c0deb
Sabina Paříková <hamanovas@gmail.com>
2023-11-28 20:13:56 +0100

    co cypress neumi, ale je dobre to vedet

---
+++ cypress/e2e/learning/cypress-hacks/tab_hack.cy.js
@@ -0,0 +1,13 @@
+describe("Tab hack checks", () => {
+  beforeEach(() => {
+    cy.visit("https://www.webdriveruniversity.com/");
+  });
+  it("Modify tab to open in same screen", () => {
+    // ! toto nebude fungovat, otevře se v nové záložce/okně: cy.get("#contact-us").click();
+    cy.get("#contact-us")
+      .scrollIntoView()
+      .invoke("removeAttr", "target")
+      .click({ force: true });
+    cy.get('input[name="first_name"]').type("Petr");
+  });
+});
